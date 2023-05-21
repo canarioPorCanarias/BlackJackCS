55844b69c56efadd8304afde8b0f877da3109bc4
javier <javierlos09876@gmail.com>
2023-05-21 23:11:23 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BlackJackCS.utils;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace BlackJackCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Deck deck = new Deck(); 
+            Image toadd = GetImage(deck.Card());
+            UserHand.Children.Add(toadd);
+            toadd.SetValue(Canvas.LeftProperty, 33d);
+            toadd.SetValue(TopProperty, 70d);
+
+
+
+        }
+        public Image GetImage(string cardname)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri($"./images/deck/{cardname}.png", UriKind.Relative);
+            bitmap.DecodePixelWidth = 55;
+            bitmap.DecodePixelHeight = 95;
+            bitmap.EndInit();
+            Image toadd = new Image
+            {
+                Source = bitmap
+            };
+            return toadd;
+        }
+
+
+
+    }
+}
---
+++ utils/Deck.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlackJackCS.utils
+{
+    internal class Deck
+    {
+        private Random rand;
+        
+        public string Card()
+        {
+            rand = new Random();
+
+            List<int[]> vect = new List<int[]>(){
+            new int []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
+            new int []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
+            new int []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
+            new int []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+            };
+
+            int typeofcard = rand.Next(1, 13) % 4;
+            int numofcard = rand.Next(1, 13) % vect[typeofcard].Where(x => x > 0).ToArray().Length;
+            while (numofcard < 0)
+            {
+                numofcard = rand.Next(1, 13) % vect[typeofcard].Where(x => x > 0).ToArray().Length;
+            }
+            vect[typeofcard][numofcard] = -1;
+            numofcard += 1;
+            typeofcard += 1;
+
+            string tipo;
+            switch (typeofcard)
+            {
+                case 1:
+                    tipo = "C";
+                    break;
+                case 2:
+                    tipo = "D";
+                    break;
+                case 3:
+                    tipo = "H";
+                    break;
+                case 4:
+                    tipo = "S";
+                    break;
+                default:
+                    tipo = "";
+                    break;
+            }
+
+            if (numofcard >= 11)
+            {
+                string grande;
+                switch (numofcard)
+                {
+                    case 11:
+                        grande = "J";
+                        break;
+                    case 12:
+                        grande = "Q";
+                        break;
+                    case 13:
+                        grande = "K";
+                        break;
+                    default:
+                        grande = "";
+                        break;
+                }
+
+                return grande + tipo + ".png";
+            }
+            else
+            {
+
+                return numofcard.ToString() + tipo + ".png";
+            }
+        }
+
+    }
+}
